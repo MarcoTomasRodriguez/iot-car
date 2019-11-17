49aff7eb19705daa8b71aa22f5b05b7c201c9aa8
Marco Tomas Rodriguez <marcotomasrodriguez@outlook.com>
2019-11-17 13:14:10 -0300

    App builded.

---
+++ app/build/precache-manifest.c89dcf63e56e48a681b78ca3afb2ae3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc012582f64f97784dfe0bb0c254466",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afbd7bee35aef00cedea",
+    "url": "/static/css/2.fefccb4d.chunk.css"
+  },
+  {
+    "revision": "1b584e2d2ec048eac4c8",
+    "url": "/static/css/main.b767310d.chunk.css"
+  },
+  {
+    "revision": "afbd7bee35aef00cedea",
+    "url": "/static/js/2.4f3cc050.chunk.js"
+  },
+  {
+    "revision": "1b584e2d2ec048eac4c8",
+    "url": "/static/js/main.da43e15a.chunk.js"
+  },
+  {
+    "revision": "f40b12b31de902f08464",
+    "url": "/static/js/runtime~main.f923b075.js"
+  }
+]);
