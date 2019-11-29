cb06491c37a47b990257febebf59d3598cdb7ca8
Marco Tomás Rodríguez <marcotomasrodriguez@gmail.com>
2019-11-29 20:23:20 -0300

    Final tests.

--- car/src/index.js
+++ car/src/index.js
@@ -2,13 +2,14 @@ const { RaspiIO } = require('raspi-io')
 const { Board } = require('johnny-five')
 const detect = require('detect-port')
 const io = require('socket.io-client')
+const localip = require('local-ip')
 const car = require('./services/car')
 const system = require('./services/system')
 
 const UI_PORT = 3000
 const SERVER_PORT = 8000
 
-const socket = io(`http://${window.location.hostname}:8000`)
+const socket = io(`http://${localip("wlan0")}:8000`)
 
 const status = async port => await detect(port) !== port ? 'running' : 'not running'
 
